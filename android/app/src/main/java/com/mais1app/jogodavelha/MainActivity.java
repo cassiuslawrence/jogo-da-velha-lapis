3d1be1362ca75ed9458c75f687d45292151c78c4
Cassius Lawrence <cassius.lawrence.contato@gmail.com>
2026-03-28 00:29:22 +0000

    feat: add Capacitor and Android support

---
+++ android/app/src/main/java/com/mais1app/jogodavelha/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mais1app.jogodavelha;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mais1app.jogodavelha',
+  appName: 'Jogo da Velha',
+  webDir: 'out'
+};
+
+export default config;
